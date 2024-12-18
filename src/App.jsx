7cbb0b3d946a90e09b77096e1a48bfb03e0823d6
Md Kaif <md.71.kaif@gmail.com>
2024-12-18 22:40:52 +0530

    Update dependencies in package-lock.json and implement color theme toggle in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Routes, Route } from "react-router-dom";
 import Messages from "./pages/Messages";
 import Login from "./pages/auth/Login";
@@ -6,12 +6,26 @@ import Signup from "./pages/auth/Signup";
 import Verification from "./pages/auth/Verification";
 
 export default function App() {
+  
+  // Check if the user has set the color theme to dark or not and apply the class to the body
+
+  useEffect(() => {
+    const colorMode = JSON.parse(localStorage.getItem("color-theme"));
+
+    const className = "dark";
+
+    const bodyClass = window.document.body.classList;
+    colorMode === "dark"
+      ? bodyClass.add(className)
+      : bodyClass.remove(className);
+  }, []);
+
   return (
     <Routes>
-      <Route index={true} element={< Messages />} />
-      <Route path="/auth/login" element= {<Login />} />
-      <Route path="/auth/signup" element= {<Signup />} />
-      <Route path="/auth/verify" element= {<Verification />} />
+      <Route index={true} element={<Messages />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/auth/verify" element={<Verification />} />
     </Routes>
   );
 }
