c7c12c0368f9409251c9c25bc17bde902aab6569
Md Kaif <md.71.kaif@gmail.com>
2024-12-25 15:51:43 +0530

    `Added toggle button and styling to UserInfo component`

--- src/section/chat/UserInfo.jsx
+++ src/section/chat/UserInfo.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { X } from "@phosphor-icons/react";
+import React from "react";
 
-export default function UserInfo() {
+export default function UserInfo(handleToggleUserInfo) {
   return (
-    <div>
-      userInfo
+    <div className="border-1 flex flex-col h-full border-stroke dark:border-strokedark">
+      <div className="sticky border-b border-stroke dark:border-strokedark flex flex-row items-center justify-between w-full px-6 py-7.5">
+        <div className="text-black dark:text-white font-semibold text-lg">
+          Profile
+        </div>
+
+        <button onClick={handleToggleUserInfo}>
+          <X size={24} />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
