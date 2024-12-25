ad74d0964053269ff842cdff0456899ea7d7cb4c
Md Kaif <md.71.kaif@gmail.com>
2024-12-25 13:58:40 +0530

    `Refactor Sidebar component: rename keys to lowercase, update icon imports, and modify navigation menu layout`

--- src/section/chat/Sidebar.jsx
+++ src/section/chat/Sidebar.jsx
@@ -1,10 +1,9 @@
 import {
   Chat,
   DotsThreeCircle,
-  Key,
   Shapes,
+  Share,
   SignOut,
-  User,
   UserCircle,
   Users,
 } from "@phosphor-icons/react";
@@ -12,60 +11,71 @@ import DarkMode from "../../components/DarkMode";
 
 const NAVIGATION = [
   {
-    Key: 0,
+    key: 0,
     title: "DMs",
-    Icon: <Chat size={24} />,
+    icon: <Chat size={24} />,
   },
   {
-    Key: 1,
+    key: 1,
     title: "Groups",
-    Icon: <Users size={24} />,
+    icon: <Users size={24} />,
   },
   {
-    Key: 2,
+    key: 2,
     title: "Profile",
-    Icon: <UserCircle size={24} />,
+    icon: <UserCircle size={24} />,
   },
   {
-    Key: 3,
+    key: 3,
     title: "More",
-    Icon: <DotsThreeCircle size={24} />,
+    icon: <DotsThreeCircle size={24} />,
   },
 ];
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col border-r border-stroke p-2 dark:border-strokedark">
-      {/* <div className="mx-auto border rounded-md border-stroke p-3 dark:border-strokedark">
+      {/* <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
         <Chat size={24} />
-      </div> */}
+      </div>*/}
 
-      <div className="flex flex-col items-center space-y-5 ">
-
+      <div className="flex flex-col items-center space-y-5">
         <div className="space-y-2 flex flex-col text-center">
           <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
-           <Shapes size={24} /> 
+            <Share size={24} />
+          </div>
+          <span className="font-medium text-sm">Workspace</span>
         </div>
 
-          {NAVIGATION.map(({Icon,Key, title}) => (
-            <div key={Key} className="space-y-2 flex flex-col text-center hover:cursor-pointer hover:text-primary">
-              {Icon}
-              <span>{title}</span>
-            </div>
-          ))}
-        </div>
-      <div className="flex flex-col grow" />
-        <span className="font-medium text-sm">Workspan</span>
+        {NAVIGATION.map(({ icon, key, title }) => (
+          <div
+            key={key}
+            className="space-y-2 flex flex-col text-center hover:coursor-pointer hover:text-primary"
+            onClick={() => {}}
+          >
+           
+           <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
+            {icon}
+
+
+
+          </div>
+          <span className="font-medium text-sm">{title}</span>
+
+
+
+          </div>
+        ))}
+      </div>
 
       <div className="flex flex-col grow"></div>
 
-      <div className="">
+      <div className="space-y-4.5">
         <DarkMode />
 
         <div className="mx-auto border rounded-md border-stroke p-3 dark:border-strokedark hover:bg-stone-300 hover:cursor-pointer">
           <SignOut size={24} />
         </div>
-      </div>
       </div>
     </div>
   );
